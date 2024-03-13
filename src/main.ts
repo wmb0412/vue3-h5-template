3e8e2d73ec035a38584de1348e74def2dd540995
wmb0412 <353685720@qq.com>
2024-03-13 17:31:03 +0800

    fix: vant dialog、notify、image-preview等样式丢失

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import { store } from "./store";
 // normalize.css
 import "normalize.css/normalize.css";
-import "vant/es/toast/style";
 
 import "@/assets/iconfont/iconfont.css";
 // 全局样式
@@ -15,6 +14,12 @@ import "virtual:svg-icons-register";
 import App from "./App.vue";
 import router from "./router";
 import { setupRouterGuard } from "./router/guard";
+
+// https://vant-contrib.gitee.io/vant/#/zh-CN/quickstart#4.-yin-ru-han-shu-zu-jian-de-yang-shi
+import "vant/es/toast/style";
+import "vant/es/dialog/style";
+import "vant/es/notify/style";
+import "vant/es/image-preview/style";
 
 async function bootstrap() {
   const app = createApp(App);
